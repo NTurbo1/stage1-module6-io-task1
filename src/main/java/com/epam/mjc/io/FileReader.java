959853b3fa1bb07a42482f7bf7916ee19759655b
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-04-29 05:07:39 +0600

    First solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        String age = null;
+        String email = null;
+        String phone = null;
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
+            name = reader.readLine().split(": ")[1];
+            age = reader.readLine().split(": ")[1];
+            email = reader.readLine().split(": ")[1];
+            phone = reader.readLine().split(": ")[1];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
     }
 }
